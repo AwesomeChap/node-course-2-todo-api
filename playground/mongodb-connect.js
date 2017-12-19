426eb922ad106ca0d57563910ad82e2cdcc6732c
Jatin Kumar <jatn15011999@gmail.com>
2017-12-20 00:54:46 +0530

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,46 @@
+// const MongoClient = require('mongodb').MongoClient;
+//
+// MongoClient.connect('mongodb://localhost:27017/TodoApp',function(err,db) {
+//     if (err) {
+//         return console.log(`Error found : ${err}`);
+//     }
+//     db.collection('Todos').insertOne({
+//         text : 'Walk the frog',
+//         completed : false
+//     },(err,res)=>{
+//         if(err){
+//             return console.log(`Error found : ${err}`);
+//         }
+//         console.log(JSON.stringify(res.ops,undefined,2));
+//     });
+//     // db.collection('Users').insertOne({
+//     //     name : 'jatin',
+//     //     age : 18,
+//     //     location : 'new delhi-110059'
+//     // }, (err,res)=>{
+//     //     if(err){
+//     //         return console.log(`Error found : ${err}`);
+//     //     }
+//     //     console.log(JSON.stringify(res.ops,undefined,2));
+//     // });
+//     db.close();
+// });
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017',(err,db)=>{
+    if(err){
+        return console.log(err);
+    }
+    db.collection('Users').insertOne({
+        name : 'Mike',
+        age : '28',
+        location : 'LA'
+    },(err,res)=>{
+        if(err){
+            return console.log(err);
+        }
+        console.log(JSON.stringify(res.ops,undefined,2));
+    });
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+// const {MongoClient, ObjectID} = require('mongodb');
+//
+// MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+//     if(err){
+//         return console.log(err);
+//     }
+//     db.collection('Todos').find({completed : true}).count().then((data)=>{
+//         console.log(JSON.stringify(data,undefined,2));
+//     },(err)=>{
+//         console.log(err);
+//     });
+//     db.collection('Todos').find({completed : true}).toArray().then((data)=>{
+//         console.log(JSON.stringify(data,undefined,2));
+//     },(err)=>{
+//         console.log(err);
+//     });
+// });
+ var {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017',(err,db)=>{
+    if(err){
+        return console.log(err);
+    }
+    db.collection('Users').find({name : 'jatin'}).count().then((count)=>{
+        console.log(`Result : ${count}`);
+    },(err)=>{
+        console.log(err);
+    });
+    db.collection('Users').find({name : 'jatin'}).toArray().then((res)=>{
+       console.log(JSON.stringify(res,undefined,2));
+    },(err)=>{
+        console.log(err);
+    });
+});
