25747fb1f308cf54348df1bb1e9a95c5624436e7
Jatin Kumar <jatn15011999@gmail.com>
2017-12-23 02:32:09 +0530

    add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose}  = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/users');
@@ -31,6 +32,24 @@ app.get('/todos',(req,res)=>{
        res.status(400).send(err);
     });
 });
+
+app.get('/todos/:id',(req,res)=>{
+
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send(' ');
+        }
+        res.status(200).send({todo});
+    }).catch((e)=>{
+        res.status(400).send('Bad Request');
+    })
+});
+
 app.listen(3000,()=>{
     console.log('On Port : 3000');
 })
