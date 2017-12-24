8c6af249731d25e1269655f3b049ff6fcaa3e8f0
Jatin Kumar <jatn15011999@gmail.com>
2017-12-24 20:45:00 +0530

    Added UPDATE /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // console.time('handler name');
-
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -72,6 +72,32 @@ app.delete('/todos/:id',(req,res)=>{
         res.status(404).send('Todo not found');
     }).catch((e)=>{
         res.status(404).send('Bad Request');
+    });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+
+    var id = req.params.id;
+    var body = _.pick(req.body,['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send('Invalid ID');
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set : body}, {new : true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send('Todo not found')
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send(e);
     });
 });
 
