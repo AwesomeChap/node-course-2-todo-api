789de5f2f055486361e5328d9aa60088c9410f06
Jatin Kumar <jatn15011999@gmail.com>
2017-12-25 01:49:28 +0530

    corrections done

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var {Todo} = require('./models/todo');
 
 //console.log(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 var app = express();
 
